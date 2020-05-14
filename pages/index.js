88df964c4ad20451888c0381b0763d988c3b197c
Jjmcclain <joshmcclain1989@gmail.com>
2020-05-14 15:20:19 -0600

    set up initial app structure

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
-import Head from 'next/head';
+import App from '../components/App';
 
 export default function Home() {
     return (
-        <div>
-            <style jsx global>{`
-                html,
-                body {
-                    padding: 0;
-                    margin: 0;
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
-                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                        Helvetica Neue, sans-serif;
-                }
-
-                * {
-                    box-sizing: border-box;
-                }
-            `}</style>
-        </div>
+        <App />
     );
 }
